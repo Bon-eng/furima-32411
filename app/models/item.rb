0cba6dc5d4ed3f6c2533cd7ff80a7020cc30505e
Bon-eng <keitaymzk@gmai.com>
2020-12-25 21:39:39 +0900

    5th commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,15 +21,15 @@ class Item < ApplicationRecord
                         greater_than_or_equal_to: 300,
                         less_than_or_equal_to: 9_999_999
                       }
-  end
-  
-  #presence: trueの入れ子にしてしまうとテストコードでわけわからなくなるので別々にして解決した
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :status_id
-    validates :postage_id
-    validates :prefecture_id
-    validates :days_to_ship_id
+                      
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :postage_id
+      validates :prefecture_id
+      validates :days_to_ship_id
+    end
+
   end
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,33 +32,33 @@ RSpec.describe Item, type: :model do
     end
 
     it 'カテゴリーの情報が必須であること' do
-      @item.category_id = 1
+      @item.category_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Category must be other than 1')
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
     end
 
     it '商品の状態についての情報が必須であること' do
-      @item.status_id = 1
+      @item.status_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Status must be other than 1')
+      expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
     end
 
     it '配送料の負担についての情報が必須であること' do
-      @item.postage_id = 1
+      @item.postage_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Postage must be other than 1')
+      expect(@item.errors.full_messages).to include("Postage can't be blank", "Postage is not a number")
     end
 
     it '発送元の地域についての情報が必須であること' do
-      @item.prefecture_id = 1
+      @item.prefecture_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
     end
 
     it '発送までの日数についての情報が必須であること' do
-      @item.days_to_ship_id = 1
+      @item.days_to_ship_id = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
+      expect(@item.errors.full_messages).to include("Days to ship can't be blank", "Days to ship is not a number")
     end
 
     it '価格についての情報が必須であること' do
@@ -70,13 +70,13 @@ RSpec.describe Item, type: :model do
     it '価格の範囲が、¥300以下だと登録できないこと' do
       @item.price = 299
       @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
     end
 
     it '価格の範囲が、¥9,999,999以上だと登録できないこと' do
       @item.price = 10_000_000
       @item.valid?
-      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
 
     it '価格について全角文字では登録できないこと' do
@@ -96,5 +96,7 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
+
   end
+
 end
