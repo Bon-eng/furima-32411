d3e3a6c7e77db1b653677c6c096bc14d370dd46c
Bon-eng <keitaymzk@gmai.com>
2021-01-06 16:52:24 +0900

    3rd commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'furima-32411'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com/Bon-eng/furima-32411.git'
+set :repo_url,  'git@github.com:Bon-eng/furima-32411.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
