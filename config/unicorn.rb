d7cd1c5df2aed43e3406ad1b342cad2df3ca35bf
Bon-eng <keitaymzk@gmai.com>
2021-01-06 12:30:43 +0900

    Auto Deploy commit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,23 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)         # 「../」が一つ増えている
 
-#アプリケーションサーバの性能を決定する（リクエストを受け付けレスポンスを生成するworker(ワーカー)の数を決める
+#アプリケーションサーバの性能を決定する
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定（UnicornがRailsのコードを動かす際、ルーティングなど実際に参照するファイルを探すディレクトリを指定
-working_directory app_path
+#アプリケーションの設置されているディレクトリを指定
+working_directory "#{app_path}/current"                   # 「current」を指定
 
-#Unicornの起動に必要なファイルの設置場所を指定（Unicornは、起動する際にプロセスidが書かれたファイルを生成する、その場所を指定する
-pid "#{app_path}/tmp/pids/unicorn.pid"
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"             # 「shared」の中を参照するよう変更
 
-#ポート番号を指定（どのポート番号のリクエストを受け付けるかを決定（Nginxを介した処理に変更したため、保存場所を3000から変更した
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+#ポート番号を指定
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"      # 「shared」の中を参照するよう変更
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
